4e536e30ec5163e9be49f02a655965e2c12937be
Kyle Coleman <kyle.coleman@slu.edu>
2018-02-16 14:34:08 -0600

    error handeling

--- Homework/ProgramOne_Sockets/Main/Client.py
+++ Homework/ProgramOne_Sockets/Main/Client.py
@@ -1,6 +1,7 @@
 import sys
 import socket
 from threading import Thread
+import select
 
 
 class Client:
@@ -8,6 +9,7 @@ class Client:
     def __init__(self, host, port):
         self._host = host
         self._port = port
+        self._read_socket = []
 
     def connect(self):
         self._client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,6 +17,7 @@ class Client:
             self._client_socket.connect((self._host, self._port))
             print('Connected to: ' + self._host)
             self._client_socket.setblocking(0)
+            self._read_socket.append(self._client_socket)
 
         except socket.error as errmsg:
             print('Caught exception socket.error: %s' % errmsg)
@@ -44,15 +47,20 @@ class Client:
         print("Beginning recieve thread...\n")
         # Poll for data
         while True:
+            ready_to_read, ready_to_write, in_error = select.select(
+                self._read_socket, [], [], 0)
             try:
-                # recv'd data must be decoded before being presented as text
-                data = self._client_socket.recv(4196).decode('UTF-8')
-                if data:
-                    print("\n")
-                    print(data)
-                    sys.stdout.write('--> ')
-                    sys.stdout.flush()
+                for s in ready_to_read:
+                    # recv'd data must be decoded before being presented as text
+                    data = self._client_socket.recv(4196).decode('UTF-8')
+                    if data:
+                        print("\n")
+                        print(data)
+                        sys.stdout.write('--> ')
+                        sys.stdout.flush()
             except socket.error as errmsg:
+                # can't print out this message because it spams constantly
+                #
                 print("Socket error caught: %s" % errmsg)
                 continue
 
