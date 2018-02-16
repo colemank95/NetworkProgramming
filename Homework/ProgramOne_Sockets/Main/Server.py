2e4517c1e74494ce84760d1df693c74392a289a6
Kyle Coleman <kyle.coleman@slu.edu>
2018-02-15 20:08:51 -0600

    updates

--- Homework/ProgramOne_Sockets/Main/Server.py
+++ Homework/ProgramOne_Sockets/Main/Server.py
@@ -3,11 +3,18 @@ import sys
 from threading import Thread
 
 # define some frequently used variables
-PORT = 58682
+PORT = 58681
 BUFFER = 4096
 # create a list to maintain all socket connections
 socket_list = []
 
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((socket.gethostname(), PORT))
+server_socket.listen(5)
+print('Socket Created')
+# this prevents us from losing our port every time we kill the process and restart it
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+socket_list.append(server_socket)
 
 def server():
     Thread(target=accept_connections).start()
@@ -36,11 +43,4 @@ def send_messages(server_socket, sender_socket, msg):
 
 
 if __name__ == "__main__":
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((socket.gethostname(), PORT))
-    server_socket.listen(5)
-    print('Socket Created')
-    # this prevents us from losing our port every time we kill the process and restart it
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    socket_list.append(server_socket)
     server()
